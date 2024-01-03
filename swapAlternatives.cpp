c3d6683013680e364edc4b0d2a48130273919cfc
Bishal-P <bishalprasad251122@gmail.com>
2024-01-04 05:18:34 +0530

    added

---
+++ swapAlternatives.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+
+using namespace std;
+
+void swapAlternatives(int arr[], int size)
+{
+    int next=0;
+    int temp;
+    for(int i=0;i<size;i=i+2){
+        next=i+1;
+        if(next<=size-1)
+        {
+            temp=arr[i];
+            arr[i]=arr[next];
+            arr[next]=temp;
+        }
+    }
+}
+
+int main(){
+    int arr[7]={1,2,3,4,5,6,7};
+    swapAlternatives(arr,7);
+    for(int i=0;i<7;i++){
+        cout<<arr[i]<<" ";
+    }
+    cout<<endl;
+    return 0;
+}

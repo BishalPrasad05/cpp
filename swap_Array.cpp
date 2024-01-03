8b56f4ddcb633cfedb19691d292b9b346020be65
Bishal-P <bishalprasad251122@gmail.com>
2024-01-03 22:54:39 +0000

    4 Jan 2024, 04:24

---
+++ swap_Array.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+
+using namespace std;
+
+void swapArray(int arr[],int size){
+    int start=0,end=size-1;
+    int temp=0;
+    while(start<end)
+    {
+        temp=arr[start];
+        arr[start]=arr[end];
+        arr[end]=temp;
+        start++;
+        end--;
+    }
+
+}
+
+int main(){
+    int arr[5]={1,2,3,4,5};
+    swapArray(arr,5);
+    for(int i=0;i<5;i++)
+    {
+        cout<<arr[i]<<" ";    
+    }
+    cout<<endl;
+    
+    return 0;
+}

27fecfc44487e7aed080dcd53d1b978b5094028a
Bishal-P <bishalprasad251122@gmail.com>
2024-01-04 18:44:37 +0000

    5 Jan 2024, 00:14

--- minMaxElement.cpp
+++ minMaxElement.cpp
@@ -1,10 +1,13 @@
 #include <iostream>
-
+#include<limits.h>
 using namespace std;
 
 int main(){
     int arr[10];
     int min=INT_MIN;
     int max=INT_MAX;
+    for(auto a;arr){
+        
+    }
     return 0;
 }

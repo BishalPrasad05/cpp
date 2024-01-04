160e0ac67dd3f1db293b019dc300a374339ec95a
Bishal-P <bishalprasad251122@gmail.com>
2024-01-04 00:16:20 +0000

    4 Jan 2024, 05:46

--- repeatedElement.cpp
+++ repeatedElement.cpp
@@ -6,11 +6,13 @@ int main(){
     int N=6;
     int arr[N]={1,2,3,4,5,2};
     int result1=0,result2=0;
-    for(int i=0;i<N-1;i++)
+    for(int i=0;i<N;i++)
     {
-        
-
-
+        if(i<=N-2){
+            result2^=i+1;            
+        }
+        result1^=arr[i]^result2;
     }
+    cout<<"The repeated element is :"<<result2<<endl;
     return 0;
 }

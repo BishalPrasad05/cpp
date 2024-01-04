2bc7d4877368d7cb166e35c257b94ce9bf13372f
Bishal-P <bishalprasad251122@gmail.com>
2024-01-04 00:18:03 +0000

    4 Jan 2024, 05:48

--- repeatedElement.cpp
+++ repeatedElement.cpp
@@ -4,15 +4,15 @@ using namespace std;
 
 int main(){
     int N=6;
-    int arr[N]={1,2,3,4,5,2};
+    int arr[N]={1,2,3,4,5,5};
     int result1=0,result2=0;
     for(int i=0;i<N;i++)
     {
         if(i<=N-2){
             result2^=i+1;            
         }
-        result1^=arr[i]^result2;
+        result1^=arr[i];
     }
-    cout<<"The repeated element is :"<<result2<<endl;
+    cout<<"The repeated element is :"<<(result1^result2)<<endl;
     return 0;
 }

789f0269a0f8da961f77ca946a0d9bcadc29a0a5
Bishal-P <bishalprasad251122@gmail.com>
2024-01-04 00:10:42 +0000

    4 Jan 2024, 05:40

---
+++ repeatedElement.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    int arr[6]={1,2,3,4,5,2};
+    int result1=0,result2=0;
+    for(int i=0;i<6;i++)
+    {
+        
+    }
+    return 0;
+}

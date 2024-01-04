8dfee46ab9bc25b3d249097ecc0f51c9cd851351
Bishal-P <bishalprasad251122@gmail.com>
2024-01-04 00:12:29 +0000

    4 Jan 2024, 05:42

--- repeatedElement.cpp
+++ repeatedElement.cpp
@@ -3,11 +3,14 @@
 using namespace std;
 
 int main(){
-    int arr[6]={1,2,3,4,5,2};
+    int N=6;
+    int arr[N]={1,2,3,4,5,2};
     int result1=0,result2=0;
-    for(int i=0;i<6;i++)
+    for(int i=0;i<N-1;i++)
     {
         
+
+
     }
     return 0;
 }

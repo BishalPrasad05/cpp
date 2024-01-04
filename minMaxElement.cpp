83b1b6b26512cf650c287c3653f995e620f7cdb6
Bishal-P <bishalprasad251122@gmail.com>
2024-01-04 18:53:05 +0000

    5 Jan 2024, 00:23

--- minMaxElement.cpp
+++ minMaxElement.cpp
@@ -3,9 +3,9 @@
 using namespace std;
 
 
-void minMax(int arr[]){
-    int arr2[]=arr;
-     int max=INT_MIN;
+int main(){
+    int arr[10]={21,32,54,6,7,4,0,234,26,65};
+    int max=INT_MIN;
     int min=INT_MAX;
     for(int a:arr){
         if(a>max)
@@ -16,10 +16,5 @@ void minMax(int arr[]){
 
     }
     cout<<"min="<<min<<" and max = "<<max<<endl;
-}
-
-int main(){
-    int arr[10]={21,32,54,6,7,4,0,234,26,65};
-    minMax(arr);
     return 0;
 }

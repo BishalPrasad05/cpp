84d9493e520cd2aa4b97d3cc6bfe41fd6b817681
Bishal-P <bishalprasad251122@gmail.com>
2024-01-05 22:11:07 +0000

    6 Jan 2024, 03:41

--- moveAnArrayByKthValue.cpp
+++ moveAnArrayByKthValue.cpp
@@ -1,28 +1,3 @@
-// #include <iostream>
-
-// using namespace std;
-
-// int main(){
-//     int arr[7]={1,2,3,4,5,6,7};
-//     int arr2[7];
-//     int k=3;
-//     int j;
-//     for(int i=0;i<=6;i++){
-//         if(i+k<=6){
-//             j=i+3;
-//         }
-//         else{
-//             j=i+(k-1)-6;
-//         }
-//         arr2[j]=arr[i];
-//     }
-    // for(int i=0;i<7;i++){
-    //     cout<<arr2[i]<<endl;
-    // }
-//     return 0;
-// }
-
-
 #include <iostream>
 
 using namespace std;

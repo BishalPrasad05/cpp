e0d92672520eff8573b4137198ebcbc56386ae9b
Bishal-P <bishalprasad251122@gmail.com>
2024-01-06 16:19:35 +0000

    6 Jan 2024, 21:49

--- kthLargestElement.cpp
+++ kthLargestElement.cpp
@@ -5,18 +5,29 @@
 
 using namespace std;
 
+
+int KthLargestElement(int arr[],int k)
+{
+    set<int> n(arr.begin(),arr.end());
+    auto it=n.begin();
+    advance(it,k);
+    // Or
+    // auto it=next(n.begin(),k);
+    return *it;
+}
+
 int main(){
-    vector<int> arr={45,23,45,2,6,34,78,678};
+    int arr[8]={45,23,45,2,6,34,78,678};
     // int n=sizeof(arr)/sizeof(arr[0]);
     // sort(arr.begin(),arr.end());
-    set<int> s(arr.begin(),arr.end());
-    for (int i:s){
-        cout<<i<<" ";
-    }
-    
-    auto it=next(s.begin(),3);
+    // set<int> s(arr.begin(),arr.end());
+    // for (int i:s){
+    //     cout<<i<<" ";
+    // }
+
+    // auto it=next(s.begin(),3);
 
     
-    cout<<*it<<endl;
+    cout<<KthLargestElement(arr,3)<<endl;
     return 0;
 }

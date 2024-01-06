4d02f5466f94a00fa6e2ff076bde25055c3a10db
Bishal-P <bishalprasad251122@gmail.com>
2024-01-06 17:09:17 +0000

    6 Jan 2024, 22:39

--- Given an array which consists of only 0, 1 and 2. Sort the array without using any sorting algo.cpp
+++ Given an array which consists of only 0, 1 and 2. Sort the array without using any sorting algo.cpp
@@ -16,13 +16,18 @@ void sortingTheArray(int arr[])
             two++;
         i++;         
     }
-    
+    for(int i=0;i<zero;i++) arr[i]=0;
+    for(int i=zero;i<one;i++) arr[i]=1;
+    for(int i=one;i<two;i++) arr[i]=2;
     cout<<size<<endl;
 }
 
 int main(){
     int arr[8]={0,0,1,0,1,0,2,2};
     sortingTheArray(arr);
+    for(int i:arr)
+        cout<<i<<" ";
+    cout<<endl;
     
     return 0;
 }

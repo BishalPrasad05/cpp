805a53799d13d9c4772f93356b667512e4952498
Bishal-P <bishalprasad251122@gmail.com>
2024-01-06 15:59:31 +0000

    6 Jan 2024, 21:29

--- kthLargestElement.cpp
+++ kthLargestElement.cpp
@@ -8,7 +8,7 @@ int main(){
     vector<int> arr={45,23,45,2,6,34,78,678};
     // int n=sizeof(arr)/sizeof(arr[0]);
     sort(arr.begin(),arr.end());
-    for(auto i,arr){
+    for(int i,arr){
         cout<<i<<" ";
     }
     cout<<endl;

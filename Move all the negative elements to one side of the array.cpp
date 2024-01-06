afb0346825db5773290548f0ce8480a88e87808e
Bishal-P <bishalprasad251122@gmail.com>
2024-01-06 17:58:06 +0000

    6 Jan 2024, 23:28

--- Move all the negative elements to one side of the array.cpp
+++ Move all the negative elements to one side of the array.cpp
@@ -2,26 +2,26 @@
 
 using namespace std;
 
-void oneSide(int arr[],int newarr[],int size)
-{
-    // int newarr[size];
-    int start=0,end=size-1;
-    for(int i=0;i<size;i++)
-    {
-        if(arr[i]>0)
-        {
-            newarr[end]=arr[i];
-            end--;
-        }
-        else{
-            newarr[start]=arr[i];
-            start++;
-        }
+// void oneSide(int arr[],int newarr[],int size)
+// {
+//     // int newarr[size];
+//     int start=0,end=size-1;
+//     for(int i=0;i<size;i++)
+//     {
+//         if(arr[i]>0)
+//         {
+//             newarr[end]=arr[i];
+//             end--;
+//         }
+//         else{
+//             newarr[start]=arr[i];
+//             start++;
+//         }
        
-    }
-    // for(int i:newarr)
-    //     cout<<i<<" ";
-}
+//     }
+//     // for(int i:newarr)
+//     //     cout<<i<<" ";
+// }
 
 
 void oneSide2(int arr[])
@@ -29,7 +29,7 @@ void oneSide2(int arr[])
     int start=0,end=7;
     while(start<end)
     {
-        if(arr[start]>0)
+        if(arr[start]>=0)
         {
             swap(arr[start],arr[end]);
             end--;
@@ -42,7 +42,7 @@ void oneSide2(int arr[])
 
 int main(){
 
-    int arr[8]={5,453,34,-1,6,-3,58,-2};
+    int arr[8]={5,453,34,-1,0,-3,58,-2};
     // int newarr[8];
     // oneSide(arr,newarr,8);
     // for(int i=0;i<8;i++)

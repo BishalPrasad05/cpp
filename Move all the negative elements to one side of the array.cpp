064febdc3b7c29ebd284939e772cbd727e8b6ac3
Bishal-P <bishalprasad251122@gmail.com>
2024-01-06 17:19:31 +0000

    6 Jan 2024, 22:49

--- Move all the negative elements to one side of the array.cpp
+++ Move all the negative elements to one side of the array.cpp
@@ -2,6 +2,26 @@
 
 using namespace std;
 
+int* oneSide(int arr[],int size)
+{
+    int newarr[size];
+    int start=0,end=size-1;
+    while(size>0)
+    {
+        if(arr[size]>0)
+        {
+            newarr[end]=arr[size];
+            end--;
+        }
+        else{
+            newarr[start]=arr[size];
+            start++;
+        }
+        size--;
+    }
+    return newarr;
+}
+
 int main(){
     
     return 0;

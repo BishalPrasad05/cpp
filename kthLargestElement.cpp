349c13544f905f0197bea7bdb9415f9f84d8f2d3
Bishal-P <bishalprasad251122@gmail.com>
2024-01-06 15:51:21 +0000

    6 Jan 2024, 21:21

--- kthLargestElement.cpp
+++ kthLargestElement.cpp
@@ -1,10 +1,11 @@
 #include <iostream>
 #include<algorithm>
+#include<vector>
 
 using namespace std;
 
 int main(){
-    int arr[8]={45,23,45,2,6,34,78,678};
+    vector<int> arr={45,23,45,2,6,34,78,678};
     arr.sort(0,7);
     return 0;
 }

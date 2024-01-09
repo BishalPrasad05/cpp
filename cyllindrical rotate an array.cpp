627a4620e5bd618b788d4e6d3ef3172b368ec1c3
Bishal-P <bishalprasad251122@gmail.com>
2024-01-09 20:21:18 +0000

    10 Jan 2024, 01:51

--- cyllindrical rotate an array.cpp
+++ cyllindrical rotate an array.cpp
@@ -2,15 +2,26 @@
 
 using namespace std;
 
-void rotate(int arr[] , int k){
-    for(int i=k,i>0;i--)
+void rotate(int arr[],int k){
+    int size=sizeof(arr);
+    int arr2[size];
+    for(int i=0;i<size;i++)
     {
-        
+        if(k==size){
+            k=0;
+        }
+        arr2[k]=arr[i];
+        k++;
     }
+    for(int i:arr2){
+        cout<<i<<" ";
+    }
+    cout<<endl;
 }
 
 int main(){
     int arr[8]={1,2,3,4,5,6,7,8};
+    rotate(arr,4);
 
     return 0;
 }

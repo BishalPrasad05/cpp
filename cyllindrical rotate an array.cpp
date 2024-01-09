bd1c3c8c79e0103a54aa2b8bc779b59cafc94adc
Bishal-P <bishalprasad251122@gmail.com>
2024-01-09 20:16:09 +0000

    10 Jan 2024, 01:46

---
+++ cyllindrical rotate an array.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+void rotate(int arr[] , int k){
+    for(int i=k,i>0;i--)
+    {
+        
+    }
+}
+
+int main(){
+    int arr[8]={1,2,3,4,5,6,7,8};
+
+    return 0;
+}

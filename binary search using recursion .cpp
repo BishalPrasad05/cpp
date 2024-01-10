2d62d702aa1e448f9ed3a23e0656dc2fffd1919e
Bishal-P <bishalprasad251122@gmail.com>
2024-01-10 22:23:53 +0530

    Fix binary search recursion bug

--- binary search using recursion .cpp
+++ binary search using recursion .cpp
@@ -2,18 +2,24 @@
 
 using namespace std;
 
-void binarySearch(int arr[],int start,int end,int key)
+int binarySearch(int arr[],int start,int end,int key)
 {
     if(start>end)
-        return;
-    int mid=(start+end)/2
+        return -1;
+    int mid=(start+end)/2;
     if(arr[mid]==key)
         return mid;
     else{
-        
+        if(key>arr[mid])
+            binarySearch(arr,mid+1,end,key);
+        else   
+            binarySearch(arr,start,mid-1,key);
     }
 }
 int main(){
+    int arr[8]={1,2,3,4,5,6,7,8};
+    int val=binarySearch(arr,0,7,100);
+    cout<<"val found at index ="<<val<<endl;
     
     return 0;
 }

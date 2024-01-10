113f957a9414bc525db054398045f31b68af9d0d
Bishal-P <bishalprasad251122@gmail.com>
2024-01-10 16:47:31 +0000

    10 Jan 2024, 22:17

---
+++ binary search using recursion .cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+void binarySearch(int arr[],int start,int end,int key)
+{
+    if(start>end)
+        return;
+    int mid=(start+end)/2
+    if(arr[mid]==key)
+        return mid;
+    else{
+        
+    }
+}
+int main(){
+    
+    return 0;
+}

68b68d50485b3b3fd76f3bc88a9c5c874345aead
Bishal-P <bishalprasad251122@gmail.com>
2024-01-10 20:58:11 +0000

    11 Jan 2024, 02:28

--- pivot element in a rotated array.cpp
+++ pivot element in a rotated array.cpp
@@ -6,14 +6,21 @@ int pivot(int arr[])
 {
     int size=sizeof(arr);
     int start=0,end=size-1,mid=(start+end)/2;
-    while(start>end)
+    while(start<end)
     {
         if(arr[mid]>=arr[0])
-            st
+            start=end+1;
+        else
+            end=mid;
+        mid=(start+end)/2;
     }
+    return end;
+
 }
 
 int main(){
+    int arr[8]={7,8,1,2,3,4,5,6};
+    cout<<pivot(arr)<<endl;
     
     return 0;
 }

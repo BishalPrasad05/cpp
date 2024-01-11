050ee484583d12582a17bc8d17efb86bea4060f2
Bishal-P <bishalprasad251122@gmail.com>
2024-01-11 13:26:35 +0000

    11 Jan 2024, 18:56

--- leftmost occurance of a digit.cpp
+++ leftmost occurance of a digit.cpp
@@ -10,8 +10,8 @@ int leftmost(int arr[],int start,int end,int ans,int key)
     int mid=(start+end)/2;
     if(arr[mid]==key){
         ans=mid;
-        end=mid-1;
-        leftmost(arr,start,end,ans,key);
+        // end=mid-1;
+        // leftmost(arr,start,end,ans,key);
     }
     else{
         if(key<=arr[mid])
@@ -23,7 +23,7 @@ int leftmost(int arr[],int start,int end,int ans,int key)
 
 int main(){
     int arr[10]={1,2,3,4,5,6,7,8,9,10};
-    int val=leftmost(arr,0,5,-1,10);
+    int val=leftmost(arr,0,5,-1,1);
     cout<<"value ="<<val<<endl;
     return 0;
 }

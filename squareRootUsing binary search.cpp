a7593c6cc54ab2a83d65853cb585dfe3e41184c0
Bishal-P <bishalprasad251122@gmail.com>
2024-01-11 19:42:31 +0000

    12 Jan 2024, 01:12

--- squareRootUsing binary search.cpp
+++ squareRootUsing binary search.cpp
@@ -5,18 +5,26 @@ using namespace std;
 
 int squareRoot(int num,int start,int end,int mid)
 {
+    mid=mid/2;
     if(mid*mid==num){
         return mid;
     }
     if(start>end)
         return mid;
-    if(mid*mid<num)
+    if(mid*mid>num)
     {
-        
+        end=mid-1;
     }
+    else{
+        start=mid+1;
+    }
+    squareRoot(num,start,end,mid);
 }
 
 int main(){
+    int num=9;
+    int val=squareRoot(num,1,num-1,-1);
+    cout<<"The value= "<<val<<endl;
     
     return 0;
 }

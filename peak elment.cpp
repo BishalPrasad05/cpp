feba7d06cb4d49bf9ee9c67b73a68f5871420f92
Bishal-P <bishalprasad251122@gmail.com>
2024-01-11 18:04:45 +0000

    11 Jan 2024, 23:34

--- peak elment.cpp
+++ peak elment.cpp
@@ -7,7 +7,9 @@ int peakElement(int arr[],int start,int end,int key)
     if(start>end)
         return key;
     int mid=(start+end)/2;
-    if(arr[mid]>)
+    if(arr[mid]>arr[mid+1])
+        end=mid-1;
+    else
 }
 
 int main(){

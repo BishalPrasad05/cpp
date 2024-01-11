3ae13b05669f5bef65920490bc6fefc16112628e
Bishal-P <bishalprasad251122@gmail.com>
2024-01-11 18:08:29 +0000

    11 Jan 2024, 23:38

--- peak elment.cpp
+++ peak elment.cpp
@@ -8,11 +8,19 @@ int peakElement(int arr[],int start,int end,int key)
         return key;
     int mid=(start+end)/2;
     if(arr[mid]>arr[mid+1])
+    {
+        key=mid;
         end=mid-1;
+    }
     else
+        start=mid+1;
+    peakElement(arr,start,end,key);
 }
 
 int main(){
+    int arr[7]={0,1,6,5,4,2,0};
+    int peak=peakElement(arr,0,2,-1);
+    cout<<"the peak element is at index :"<<peak<<endl;
     
     return 0;
 }

8b481f66d24a1d2384d9c96c4132db6c86926d7e
Bishal-P <bishalprasad251122@gmail.com>
2024-01-11 18:30:53 +0000

    12 Jan 2024, 00:00

--- search element in rotated array.cpp
+++ search element in rotated array.cpp
@@ -2,14 +2,19 @@
 
 using namespace std;
 
-// 7,9,1,2,3
+// 6,7,9,1,2,3,4
 
 int searchInRotatedArray(int arr,int start,int end,int key)
 {
     if(start>end)
         return key;
     int mid=(start+end)/2;
+    if(arr[mid]==key)
+        return mid;
     if(arr[mid]>key){
+        end=mid-1;
+    }
+    else{
         if()
     }
 }

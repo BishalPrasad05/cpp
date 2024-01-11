43e8d2ab7a071d10dbd836c4ba053cb72c881de7
Bishal-P <bishalprasad251122@gmail.com>
2024-01-11 18:00:09 +0000

    11 Jan 2024, 23:30

---
+++ peak elment.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int peakElement(int arr[],int start,int end,int key)
+{
+    if(start>end)
+        return key;
+    int mid=(start+end)/2;
+    if(arr[mid]>)
+}
+
+int main(){
+    
+    return 0;
+}

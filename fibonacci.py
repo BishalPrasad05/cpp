7ec5763246f0f23176885cd2a693d74b8114cc2b
Bishal-P <bishalprasad251122@gmail.com>
2024-01-12 16:21:39 +0000

    12 Jan 2024, 21:51

--- fibonacci.py
+++ fibonacci.py
@@ -1,8 +1,8 @@
-def fibo(first,second,k):
-    if k==0:
-        return
-    else:
-        print(first)
-        fibo(second,first+second,k-1)
+# def fibo(first,second,k):
+#     if k==0:
+#         return
+#     else:
+#         print(first)
+#         fibo(second,first+second,k-1)
 
-fibo(0,1,10)
+# fibo(0,1,10)

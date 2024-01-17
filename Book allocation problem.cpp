433c3539fbd95d813dde203108df269f0e9d149c
Bishal-P <bishalprasad251122@gmail.com>
2024-01-17 13:14:44 +0000

    17 Jan 2024, 18:44

--- Book allocation problem.cpp
+++ Book allocation problem.cpp
@@ -4,6 +4,29 @@ using namespace std;
 
 int main(){
     int start=90,end=203,mid,ans;
-    int arr[]
+    int arr[4]={};
+    while(start<=end)
+    {
+        mid=(start+end)/2;
+        page=0;
+        count=1;
+        for(int i=0;i<4;i++)
+        {
+            page+=arr[i];
+            if(page>mid)
+            {
+                count++;
+                page=arr[i];
+            }
+        }
+        if(count<=2)
+        {
+            ans=mid;
+            end=mi-1;
+        }
+        else{
+            start=mid+1;
+        }
+    }
     return 0;
 }

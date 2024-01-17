a6950d704279f5a3284746af2f13e935c8e13f0f
Bishal-P <bishalprasad251122@gmail.com>
2024-01-17 13:19:00 +0000

    17 Jan 2024, 18:49

--- Book allocation problem.cpp
+++ Book allocation problem.cpp
@@ -1,33 +1,44 @@
+// #include <iostream>
+
+// using namespace std;
+
+// int main(){
+//     int start=90,end=203,mid,ans;
+//     int arr[4]={12,34,67,90};
+//     while(start<=end)
+//     {
+//         mid=(start+end)/2;
+//         int page=0;
+//         int count=1;
+//         for(int i=0;i<4;i++)
+//         {
+//             page+=arr[i];
+//             if(page>mid)
+//             {
+//                 count++;
+//                 page=arr[i];
+//             }
+//         }
+//         if(count<=2)
+//         {
+//             ans=mid;
+//             end=mid-1;
+//         }
+//         else{
+//             start=mid+1;
+//         }
+//     }
+//     cout<<"The answer is :"<<ans<<endl;
+//     return 0;
+// }
+
+
+
 #include <iostream>
 
 using namespace std;
 
 int main(){
-    int start=90,end=203,mid,ans;
-    int arr[4]={12,34,67,90};
-    while(start<=end)
-    {
-        mid=(start+end)/2;
-        int page=0;
-        int count=1;
-        for(int i=0;i<4;i++)
-        {
-            page+=arr[i];
-            if(page>mid)
-            {
-                count++;
-                page=arr[i];
-            }
-        }
-        if(count<=2)
-        {
-            ans=mid;
-            end=mid-1;
-        }
-        else{
-            start=mid+1;
-        }
-    }
-    cout<<"The answer is :"<<ans<<endl;
+    int start=90,end=;
     return 0;
 }

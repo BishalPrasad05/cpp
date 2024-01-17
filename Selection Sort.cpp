fb2982edbcf31dc80cfd485a070954dd4b3967f6
Bishal-P <bishalprasad251122@gmail.com>
2024-01-17 22:43:40 +0000

    18 Jan 2024, 04:13

--- Selection Sort.cpp
+++ Selection Sort.cpp
@@ -19,23 +19,17 @@ void selectionSort(int arr[],int size)
 
 int main(){
     int arr[5]={5,4,3,2,1};
-    selectionSort(arr,5);
+    // selectionSort(arr,5);
     cout<<"The sorted array"<<endl;
     for(int i=0;i<5-1;i++)
     {
-        int j=i;
         int temp=i;
-        for(j;j<5;j++)
+        for(int j=i;j<5;j++)
         {
-            if(arr[j]>arr[i])
+            if(arr[j]<arr[i])
              temp=j;
         }
-        int temp2=arr[i];
-        arr[i]=arr[temp];
-        arr[temp]=temp2;
-
-        
-        
+        swap(arr[temp],arr[i]); 
     }
 
 

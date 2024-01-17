091432a5a33f307da2d18834f3deba293695508d
Bishal-P <bishalprasad251122@gmail.com>
2024-01-17 13:47:17 +0000

    17 Jan 2024, 19:17

--- Book allocation problem.cpp
+++ Book allocation problem.cpp
@@ -1,39 +1,3 @@
-// #include <iostream>
-
-// using namespace std;
-
-// int main(){
-//     int start=90,end=203,mid,ans;
-//     int arr[4]={12,34,67,90};
-//     while(start<=end)
-//     {
-//         mid=(start+end)/2;
-//         int page=0;
-//         int count=1;
-//         for(int i=0;i<4;i++)
-//         {
-//             page+=arr[i];
-//             if(page>mid)
-//             {
-//                 count++;
-//                 page=arr[i];
-//             }
-//         }
-//         if(count<=2)
-//         {
-//             ans=mid;
-//             end=mid-1;
-//         }
-//         else{
-//             start=mid+1;
-//         }
-//     }
-//     cout<<"The answer is :"<<ans<<endl;
-//     return 0;
-// }
-
-
-
 #include <iostream>
 
 using namespace std;
@@ -66,8 +30,6 @@ int main(){
     while(start<=end)
     {
         int mid=(start+end)/2;
-        int value=isFeasible(arr,mid);
-        cout<<"The value is :"<<value<<endl;
         if(isFeasible(arr,mid)){
             ans=mid;
             end=mid-1;

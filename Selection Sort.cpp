783a5ec1faf6a8da45fb583834322be8124e5652
Bishal-P <bishalprasad251122@gmail.com>
2024-01-17 22:32:58 +0000

    18 Jan 2024, 04:02

--- Selection Sort.cpp
+++ Selection Sort.cpp
@@ -7,10 +7,12 @@ void selectionSort(int arr[],int size)
     int temp=0;
     for(int i=0;i<size-1;i++){
         int j=i;
-        for(j;j<size-1;j++)
+        for(j+1;j<size-1;j++)
         {
-            if()
+            if(arr[j]<arr[i])
+             temp=arr[j];
         }
+        swap(arr[i],temp)
     }
 
 }

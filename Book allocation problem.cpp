2c86dbcc2745df8d7ef019d1aefe7500fedb424f
Bishal-P <bishalprasad251122@gmail.com>
2024-01-17 07:58:06 +0000

    17 Jan 2024, 13:28

--- Book allocation problem.cpp
+++ Book allocation problem.cpp
@@ -2,17 +2,24 @@
 
 using namespace std;
 
-int* minMax(int arr[],int ){
+void minMax(int arr[],int minMaxArr[]){
     int size=sizeof(arr);
-
+    int max=0;
     for(int i=0 ;i<size;i++)
     {
-
+        if(arr[i]>max){
+            minMaxArr[0]=arr[i];
+            max=arr[i];
+        }
+        minMaxArr[1]+=arr[i];
     }
 }
 
 
 int main(){
+    int arr[]={10,10,20,30};
+    int minMaxArr[2]={0};
+    minMax(arr,minMaxArr);
     
     return 0;
 }

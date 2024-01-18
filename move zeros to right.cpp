5c6efd75cfc1ff7f34f6f0287ae5769159fda36b
Bishal-P <bishalprasad251122@gmail.com>
2024-01-18 13:29:04 +0000

    18 Jan 2024, 18:59

---
+++ move zeros to right.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    int arr[8]={1,0,3,9,0,6,0,0};
+    int start=0,end=7;
+    while(start<end)
+    {
+        
+    }
+    return 0;
+}

b30b61ed0f9c823b85c38aec912197a18b6b3757
Bishal-P <bishalprasad251122@gmail.com>
2024-01-18 12:04:55 +0000

    18 Jan 2024, 17:34

---
+++ bubble sort.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    int arr[8]={5,4,4,67,98,90,435,2};
+    for(int i=0 ;i<8;i++)
+    {
+        for(int j=0;j<7-i;j++)
+        {
+            if(arr[j])
+        }
+    }
+    return 0;
+}

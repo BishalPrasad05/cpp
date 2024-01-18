a2d0618e23c6797b01ed321ab787d8d2cae969e2
Bishal-P <bishalprasad251122@gmail.com>
2024-01-18 12:34:34 +0000

    18 Jan 2024, 18:04

--- insertion sort .cpp
+++ insertion sort .cpp
@@ -6,10 +6,16 @@ int main(){
     int arr[8]={5,4,4,67,98,90,435,2};
     for(int i=1;i<8;i++)
     {
+        int temp=i;
         for(int j=i;j>0;j--)
         {
-            if(arr[i]<arr)
+            if(arr[j]>arr[i])
+                temp=j;
         }
+        swap(arr[temp],arr[i]);
     }
+    for(int var:arr)
+        cout<<var<<" ";
+    cout<<endl;
     return 0;
 }

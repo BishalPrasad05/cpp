591e6a69d206add37eaf9f1db4429123409d93cc
Bishal-P <bishalprasad251122@gmail.com>
2024-01-18 22:36:02 +0000

    19 Jan 2024, 04:06

--- sorted and rotated array.cpp
+++ sorted and rotated array.cpp
@@ -3,6 +3,12 @@
 using namespace std;
 
 int main(){
-    int arr[7]={4,5,6,1,2,3,4}
+    int arr[7]={4,5,6,1,2,3,4};
+    int count=0;
+    for(int i=1;i<7;i++)
+    {
+        if(arr[i-1]>arr[i])
+                       
+    }
     return 0;
 }

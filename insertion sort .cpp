1b8325013597004c3f32598a4a0fdf8bfa279cb3
Bishal-P <bishalprasad251122@gmail.com>
2024-01-18 12:31:56 +0000

    18 Jan 2024, 18:01

--- insertion sort .cpp
+++ insertion sort .cpp
@@ -3,6 +3,13 @@
 using namespace std;
 
 int main(){
-    
+    int arr[8]={5,4,4,67,98,90,435,2};
+    for(int i=1;i<8;i++)
+    {
+        for(int j=i;j>0;j--)
+        {
+            
+        }
+    }
     return 0;
 }

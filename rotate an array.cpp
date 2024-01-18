915485906b5d0014a18e94e83e747b072b9a8870
Bishal-P <bishalprasad251122@gmail.com>
2024-01-18 13:56:16 +0000

    18 Jan 2024, 19:26

--- rotate an array.cpp
+++ rotate an array.cpp
@@ -3,9 +3,9 @@
 
 using namespace std;
 
-void reverseArrayByKth(vector<int> arr[],int k)
+void reverseArrayByKth(vector<int> arr,int k)
 {
-    vector<int> arr2[arr.size()];
+    vector<int> arr2(arr.size());
     for(int i=0;i<arr.size();i++)
     {
         arr2[(i+k)%arr.size()]=arr[i];
@@ -14,7 +14,7 @@ void reverseArrayByKth(vector<int> arr[],int k)
 }
 
 int main(){
-    vector<int> arr[5]={1,2,3,4,5};
+    vector<int> arr={1,2,3,4,5};
     reverseArrayByKth(arr,2);
     for(int var: arr)
     {

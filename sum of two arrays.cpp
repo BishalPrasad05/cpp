df83dcc72565c72a819df0ffd17ed36a83db9558
Bishal-P <bishalprasad251122@gmail.com>
2024-01-19 07:04:19 +0000

    19 Jan 2024, 12:34

--- sum of two arrays.cpp
+++ sum of two arrays.cpp
@@ -7,7 +7,7 @@ int main(){
     int arr2[2]={1,1};
     int p1=3,p2=1,rem=0,carry=0;
     for(int i=0;i<4;i++){
-        rem=(arr1[p1]+arr2[p2]+)/10;
+        rem=(arr1[p1]+arr2[p2]+rem)/10;
         arr1[p1]=(arr1[p1]+arr2[p2]+carry)%10;
 
     }

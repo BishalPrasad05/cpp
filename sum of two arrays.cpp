d8027857fe3afaea60741841d19d527cfe31cc64
Bishal-P <bishalprasad251122@gmail.com>
2024-01-19 07:32:55 +0000

    19 Jan 2024, 13:02

--- sum of two arrays.cpp
+++ sum of two arrays.cpp
@@ -3,7 +3,7 @@
 using namespace std;
 
 int main(){
-    int arr1[4]={1,2,3,4};
+    int arr1[4]={1,5,3,4};
     int arr2[2]={8,9};
     int p1=3,p2=1,rem=0,carry=0,down=0;
     for(int i=0;i<3;i++){

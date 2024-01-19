a89b5ce813a5839ec352c4e45c2df96f04039b4e
Bishal-P <bishalprasad251122@gmail.com>
2024-01-19 07:32:19 +0000

    19 Jan 2024, 13:02

--- sum of two arrays.cpp
+++ sum of two arrays.cpp
@@ -6,7 +6,7 @@ int main(){
     int arr1[4]={1,2,3,4};
     int arr2[2]={8,9};
     int p1=3,p2=1,rem=0,carry=0,down=0;
-    for(int i=0;i<2;i++){
+    for(int i=0;i<3;i++){
         cout<<rem<<endl;
         if(p2>=0)
             down=arr2[p2];

2c9a92ad3d0414c986df57210436267a940da337
Bishal-P <bishalprasad251122@gmail.com>
2024-01-19 06:42:31 +0000

    19 Jan 2024, 12:12

--- sum of two arrays.cpp
+++ sum of two arrays.cpp
@@ -5,7 +5,10 @@ using namespace std;
 int main(){
     int arr1[4]={1,2,3,4};
     int arr2[2]={1,1};
-    for(int i=0;i)
+    int p1=3,p2=1,rem=0;
+    for(int i=0;i<4;i++){
+        
+    }
 
     return 0;
 }

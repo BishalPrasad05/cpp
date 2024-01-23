b551790992195b6a02f43471c5fdccb0d1c20e47
Bishal-P <bishalprasad251122@gmail.com>
2024-01-23 09:12:54 +0000

    23 Jan 2024, 14:42

---
+++ character to lowercase or uppercase.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+char toLowercase(int ch)
+{
+    if(ch>'a' && ch<'z')
+        return ch;
+    else{
+        int res=ch
+    }
+}
+
+
+int main(){
+    
+    return 0;
+}

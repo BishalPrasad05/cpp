ba85cb54b4bb4b3ec45db244ac4fdcf9d9061d80
Bishal-P <bishalprasad251122@gmail.com>
2024-01-28 23:33:21 +0000

    29 Jan 2024, 05:03

--- reverse words in a sentence.cpp
+++ reverse words in a sentence.cpp
@@ -16,7 +16,7 @@ int main(){
     string word="Hello how are you";
     for(int i=0;i<word.length();i++)
     {
-        cout<<word[i]<<endl;
+        cout<<typeid(word[i]).name()<<endl;
     }
     // cout<<word[0:5]<<endl;
     return 0;

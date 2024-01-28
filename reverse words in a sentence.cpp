475910c1f91ed65888465a45ff495b64e716f44a
Bishal-P <bishalprasad251122@gmail.com>
2024-01-28 23:18:35 +0000

    29 Jan 2024, 04:48

--- reverse words in a sentence.cpp
+++ reverse words in a sentence.cpp
@@ -2,16 +2,20 @@
 
 using namespace std;
 
-String reverseWord(String ,int start,int end)
-{
-    while(start<end)
-    {
+// String reverseWord(String ,int start,int end)
+// {
+//     while(start<end)
+//     {
         
-    }  
-    return 
-}
+//     }  
+//     return 
+// }
 
 int main(){
-    
+    String word="Hello how are you";
+    for(int i=0;i<word.len;i++)
+    {
+        cout<<i<<endl;
+    }
     return 0;
 }

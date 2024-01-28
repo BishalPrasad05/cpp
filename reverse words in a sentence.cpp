2cf60cd9ee9e811ec237f09a5266868481ade59f
Bishal-P <bishalprasad251122@gmail.com>
2024-01-28 23:40:36 +0000

    29 Jan 2024, 05:10

--- reverse words in a sentence.cpp
+++ reverse words in a sentence.cpp
@@ -16,12 +16,14 @@ using namespace std;
 int main(){
     string word="Hello how are you";
     int s=0,e=0;
+    int previous_space=0;
     for(int i=0;i<word.length();i++)
     {
         // cout<<typeid(word[i]).name()<<endl;
         if(isspace(word.at(i)))
         {
-            
+
+
         }
     }
     // cout<<word[0:5]<<endl;

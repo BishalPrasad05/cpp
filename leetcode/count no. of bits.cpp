556fe672e68da5a2d0d48e99ed460769048ebd36
Bishal-P <bishalprasad251122@gmail.com>
2024-02-07 23:06:28 +0000

    8 Feb 2024, 04:36

--- leetcode/count no. of bits.cpp
+++ leetcode/count no. of bits.cpp
@@ -9,19 +9,19 @@ int main(){
     cout<<"yoooooooooo";
     for(int i=0;i<=n;i++)
     {
-        cout<<"yes";
+        
         int count1=0;
         int j=i;
         while(j>0)
         {
-            cout<<"no";
+          
             if(j&1)
             count1++;
             j=j>>1;
         }
         bits[i]=count1;
     }
-    for(int i=0;i<=n;i++)
-    cout<<bits[i]<<endl;
+    // for(int i=0;i<=n;i++)
+    // cout<<bits[i]<<endl;
     return 0;
 }

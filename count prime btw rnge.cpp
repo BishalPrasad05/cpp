6813d214cea2dd4912f770ce86b3eebf21bbed9c
Bishal-P <bishalprasad251122@gmail.com>
2024-02-07 22:08:46 +0000

    8 Feb 2024, 03:38

---
+++ count prime btw rnge.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    
+    int count=0,n=10;
+
+    vector<int> prime(n-1,true);
+    primt[0]=prime[1]=0;
+    for(int i=2;i<n;i++)
+    {
+        if(prime[i])
+        count++;
+        for(int )
+    }
+    return 0;
+}

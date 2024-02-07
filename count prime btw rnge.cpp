20292664123647fb6d48d15627cf7ccc6874145c
Bishal-P <bishalprasad251122@gmail.com>
2024-02-07 22:13:32 +0000

    8 Feb 2024, 03:43

--- count prime btw rnge.cpp
+++ count prime btw rnge.cpp
@@ -7,12 +7,12 @@ int main(){
     int count=0,n=10;
 
     vector<int> prime(n-1,true);
-    primt[0]=prime[1]=0;
+    prime[0]=prime[1]=0;
     for(int i=2;i<n;i++)
     {
         if(prime[i])
         count++;
-        for(int j=2*i;j<n;j+i)
+        for(int j=2*i;j<n;j=j+i)
         prime[j]=0;
     }
     cout<<"The no of prime numbers are :"<<count<<endl;

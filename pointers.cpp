20ae75a39dee2dde6f4c47d67d05468b9302fc7e
Bishal-P <bishalprasad251122@gmail.com>
2024-02-12 12:38:27 +0000

    12 Feb 2024, 18:08

---
+++ pointers.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    int num=6;
+    cout<<"The value of num :"<<num<<endl;
+    cout<<"The address of num :"<<&num<<endl;
+    int *p=0;
+    p=&num;
+    cout<<"The pointer value of p :"<<p<<endl;
+    return 0;
+}

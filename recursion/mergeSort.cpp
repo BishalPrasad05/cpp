f601fa89b11f3f1658d3abaa916ce3d20626c71e
Bishal-P <bishalprasad251122@gmail.com>
2024-03-05 21:24:30 +0000

    6 Mar 2024, 02:54

--- recursion/mergeSort.cpp
+++ recursion/mergeSort.cpp
@@ -2,28 +2,11 @@
 
 using namespace std;
 
-void mergeSort(int *arr, int s, int e)
-{
-    if (s > e)
-    {
-        return;
-    }
-    // for (int i = s; i < e; i++)
-    //     cout << arr[i] << " ";
-    // cout << endl;
-
-    int mid = (s + e) / 2;
-    cout << mid << endl;
-    if (mid == 0)
-        return;
-    mergeSort(arr, s, mid);
-    mergeSort(arr, mid + 1, e);
-}
 
 int main()
 {
     int arr[5] = {5, 4, 3, 2, 1};
-    mergeSort(arr, 0, 4);
+    mergeSort1(arr, 0, 4);
 
     return 0;
 }

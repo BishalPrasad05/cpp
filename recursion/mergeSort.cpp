a8e7dc887e16a3c21e1e7afaf567647cd693c1cc
Bishal-P <bishalprasad251122@gmail.com>
2024-03-05 21:42:24 +0000

    6 Mar 2024, 03:12

--- recursion/mergeSort.cpp
+++ recursion/mergeSort.cpp
@@ -4,33 +4,34 @@ using namespace std;
 
 int arr2[5];
 
-void Merge(int arr[], int s, int e)
+void Merge(int arr[], int start, int end)
 {
-    if (s == e)
-    {
-        cout << arr[s] << endl;
-        return;
-    }
-    for (int i = s; i <= e; i++)
-        cout << arr[i] << " ";
-    cout << endl;
+    cout << arr[start] << " " << arr[end] << endl;
+    // if (s == e)
+    // {
+    //     cout << arr[s] << endl;
+    //     return;
+    // }
+    // for (int i = s; i <= e; i++)
+    //     cout << arr[i] << " ";
+    // cout << endl;
 }
 
 void MSort(int arr[], int start, int end)
 {
-    cout << arr[start] << " " << arr[end] << endl;
+    // cout << arr[start] << " " << arr[end] << endl;
     if (start >= end)
         return;
     int mid = (start + end) / 2;
 
     MSort(arr, start, mid);
     MSort(arr, mid + 1, end);
-    // Merge(arr, start, end);
+    Merge(arr, start, end);
 }
 
 int main()
 {
-    int arr[5] = {5, 4, 3, 2, 1};
+    int arr[5] = {5, 4, 3, 2, 1,0};
     MSort(arr, 0, 4);
 
     return 0;

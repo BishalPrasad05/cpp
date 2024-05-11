0430bab69af7abb787067dcad77849b58b6208c5
Bishal-P <bishalprasad251122@gmail.com>
2024-05-11 12:39:22 +0000

    11 May 2024, 18:09

--- raj_project.ino
+++ raj_project.ino
@@ -1,5 +1,4 @@
-
-
+#include <Servo.h>
 
 void setup()
 {
---
+++ tree/binaryTree.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+
+using namespace std;
+
+class node{
+    public:
+        int data;
+        node *left;
+        node *right;
+    node(int data)
+    {
+        this->data = data;
+        this->left = NULL;
+        this->right = NULL;
+    }
+};
+
+
+node* binaryTree(node* root)
+{
+    cout << "Enter the data:";
+    int data;
+    cin >> data;
+    root = new node(data);
+    if (data == -1)
+        return NULL;
+
+    cout << "Enter the left of " << data << " " << endl;
+    binaryTree(root->left);
+    
+}
+
+int main(){
+    
+    return 0;
+}
